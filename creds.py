2e60f6b0106fc6a9ceab8a08e24f6415365775a0
Itzmecp <121564783+Cpmodzyt@users.noreply.github.com>
2024-10-26 11:22:00 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -11,7 +11,6 @@ class cred:
         "25812247"
     )  # "Get this value from my.telegram.org! Please do not steal"
     API_HASH = os.getenv(
-        "00a50dc03d5b63a0eabfca83fa02c4ca
-"
+        "00a50dc03d5b63a0eabfca83fa02c4ca"
     )  # "Get this value from my.telegram.org! Please do not steal"
     DB_URL = os.getenv("mongodb+srv://AnimeFlix:Itzmecp@cluster0.qxdxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # From Firebase database
