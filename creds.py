45e2a086cd9f0fe6443b61409039bf247dfe6a7d
Itzmecp <121564783+Cpmodzyt@users.noreply.github.com>
2024-10-26 11:14:19 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,12 +5,13 @@ load_dotenv(find_dotenv())
 
 
 class cred:
-    BOT_NAME = os.getenv("BOT_NAME")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")  # From botfather
+    BOT_NAME = os.getenv("testanimeflix")
+    BOT_TOKEN = os.getenv("7820025967:AAGeYjX4wno2aFfTE9w6VHm6Ur6VFWa_2ns")  # From botfather
     API_ID = os.getenv(
-        "API_ID"
+        "25812247"
     )  # "Get this value from my.telegram.org! Please do not steal"
     API_HASH = os.getenv(
-        "API_HASH"
+        "00a50dc03d5b63a0eabfca83fa02c4ca
+"
     )  # "Get this value from my.telegram.org! Please do not steal"
-    DB_URL = os.getenv("DB_URL")  # From Firebase database
+    DB_URL = os.getenv("mongodb+srv://AnimeFlix:Itzmecp@cluster0.qxdxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # From Firebase database
